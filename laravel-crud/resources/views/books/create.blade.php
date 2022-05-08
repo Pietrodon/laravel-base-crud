0082b0f1864c568a5d352ddbe53d6f7f821147c4
Pietrodon <donvitopietro00@gmail.com>
2022-05-08 22:26:08 +0200

    modifiche index

--- laravel-crud/resources/views/books/create.blade.php
+++ laravel-crud/resources/views/books/create.blade.php
@@ -1,20 +1,14 @@
-{{-- <form action="{{route('books.store')}}" method="POST">
-    @csrf
-    <input type="text" name="title" placeholder="Titolo del libro">
-    <textarea name="plot" id="plot" cols="30" rows="10" placeholder="Trama del libro"></textarea>
-    <input type="submit" value="Crea">
-</form> --}}
-<form action="{{route('books.store')}}" method="POST">
-    @csrf
+@extends('layouts.app')
 
-    <div class="form-group">
-      <label for="title">Titolo del Fumetto</label>
-      <input type="text" class="form-control" id="title" name="title" aria-describedby="emailHelp">
-    </div>
+@section('content')
+<div class="container">
+    <form action="{{route('books.store')}}" method="POST">
+        @csrf
+        <input type="text" name="title" placeholder="Titolo del libro">
+        <textarea name="plot" id="plot" cols="30" rows="10" placeholder="Trama del libro"></textarea>
+        <input type="submit" value="Crea">
+    </form>
+</div>
 
-    <div class="form-group">
-      <label for="plot">Trama del Libro</label>
-      <textarea class="form-control" id="plot" name="plot" rows="3"></textarea>
-    </div>
-    <button type="submit" class="btn btn-primary">Crea</button>
-  </form>
+@endsection
+
--- laravel-crud/resources/views/books/index.blade.php
+++ laravel-crud/resources/views/books/index.blade.php
@@ -1,8 +1,19 @@
-<div>
-    <ul>
-        @foreach ($books as $book)
-            <li>{{$book->title}}</li>
-            <li>{{$book->plot}}</li>
-        @endforeach
-    </ul>
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+    @foreach ($books as $book)
+    <div class="card" style="width: 18rem;">
+        <div class="card-body">
+          <h5 class="card-title">{{$book->title}}</h5>
+          <p class="card-text">{{$book->plot}}</p>
+          <a href="#" class="card-link">Modifica</a>
+          <a href="#" class="card-link">Elimina</a>
+        </div>
+    </div>
+@endforeach
 </div>
+
+
+@endsection
+
