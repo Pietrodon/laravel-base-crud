733bbf4c33ec8b0eeb42813151b149de2949442f
Pietrodon <donvitopietro00@gmail.com>
2022-05-08 21:11:34 +0200

    init repo

---
+++ laravel-crud/resources/views/books/create.blade.php
@@ -0,0 +1,6 @@
+<form action="">
+    @csrf
+    <input type="text" name="title" placeholder="Titolo del libro">
+    <textarea name="plot" id="plot" cols="30" rows="10" placeholder="Trama del libro"></textarea>
+    <input type="submit" value="Crea">
+</form>
